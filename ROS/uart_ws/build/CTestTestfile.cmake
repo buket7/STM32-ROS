a25f4ba8354b77a9326aa7a03e0000bdbed9a4be
buket7 <67732340+buket7@users.noreply.github.com>
2022-04-08 02:39:57 +0300

    Add files via upload

---
+++ ROS/uart_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros-7/uart_ws/src
+# Build directory: /home/ros-7/uart_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("uart_pkg")
